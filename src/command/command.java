3974f3ffde9eb89a0dfd177f322162df2a994517
Isabella3003 <62352111+Isabella3003@users.noreply.github.com>
2020-05-04 13:05:53 -0300

    Create command.java

---
+++ src/command/command.java
@@ -0,0 +1,13 @@
+comando do pacote ;
+
+importar  java.io.IOException ;
+
+importar  javax.servlet.ServletException ;
+importar  javax.servlet.http.HttpServletRequest ;
+importar  javax.servlet.http.HttpServletResponse ;
+
+ Comando de interface  pública {
+
+	pública  vazio  executar ( HttpServletRequest  pedido , HttpServletResponse  resposta ) lança  ServletException , IOException ;
+
+}
